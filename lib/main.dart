b0604d0217cb032f83fdd5bab943d90c8daa7d6e
yuumuJS <yuumuknn@gmail.com>
2023-03-09 17:51:33 +0900

    色変更

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const TopPage(),
     );
